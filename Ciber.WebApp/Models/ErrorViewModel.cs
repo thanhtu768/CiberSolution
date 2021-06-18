ef7b7e64dce411689dad11a7659ba1472df0a949
TuLT <TuLT@DESKTOP-96MKO36>
2021-06-18 23:19:48 +0700

    Add project files.

---
+++ Ciber.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ciber.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
